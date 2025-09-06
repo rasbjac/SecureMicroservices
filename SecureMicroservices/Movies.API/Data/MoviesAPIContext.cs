c27bad74980e8d04975b6129cdca86d59c53f619
Rafael Santos <rasb.jac@gmail.com>
2025-09-06 08:30:27 -0300

    Adding Movies API project
    Adding Movies API project

---
+++ SecureMicroservices/Movies.API/Data/MoviesAPIContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Movies.API.Data
+{
+    public class MoviesAPIContext : DbContext
+    {
+        public MoviesAPIContext (DbContextOptions<MoviesAPIContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Model.Movie> Movie { get; set; } = default!;
+    }
+}
---
+++ SecureMicroservices/Movies.API/Program.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Movies.API.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<MoviesAPIContext>(options =>
+    options.UseInMemoryDatabase("Movies"));
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+SeedDatabase(app);
+
+void SeedDatabase(IHost app)
+{
+    using (var scope = app.Services.CreateScope())
+    {
+        var movieContext = scope.ServiceProvider.GetRequiredService<MoviesAPIContext>();
+        MoviesContextSeed.SeedAsync(movieContext);
+    }
+}
+
+app.Run();
