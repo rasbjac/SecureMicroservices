9d16b7938fe643094b15af8837cfdb9d1a1e8348
Rafael Santos <rasb.jac@gmail.com>
2025-09-06 08:58:30 -0300

    Starting IdentityServer
    Starting IdentityServer

--- SecureMicroservices/IdentityServer/Program.cs
+++ SecureMicroservices/IdentityServer/Program.cs
@@ -1,5 +1,19 @@
+using Duende.IdentityServer.Models;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddIdentityServer()
+    .AddInMemoryClients(new List<Client>())
+    .AddInMemoryIdentityResources(new List<IdentityResource>())
+    .AddInMemoryApiResources(new List<ApiResource>())
+    .AddInMemoryApiScopes(new List<ApiScope>())
+    .AddTestUsers(new List<Duende.IdentityServer.Test.TestUser>())
+    .AddDeveloperSigningCredential();
+
+
 var app = builder.Build();
+
+app.UseRouting();
+app.UseIdentityServer();
 
 app.MapGet("/", () => "Hello World!");
 
