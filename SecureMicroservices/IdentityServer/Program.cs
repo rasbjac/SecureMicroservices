333546b4c3b0c364fd8c26251ed9f92739ff258e
Rafael Santos <rasb.jac@gmail.com>
2025-10-15 23:02:10 -0300

    commit: adding Duende Quick Start Pages.

--- SecureMicroservices/IdentityServer/Program.cs
+++ SecureMicroservices/IdentityServer/Program.cs
@@ -6,12 +6,33 @@ builder.Services.AddIdentityServer()
     .AddInMemoryApiScopes(Config.ApiScopes)
     .AddDeveloperSigningCredential();
 
+builder.Services.AddRazorPages();
 
 var app = builder.Build();
 
+// --- Configure middleware pipeline ---
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+else
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
 app.UseRouting();
+
+// This is important: UseIdentityServer includes authentication middleware needed by it
 app.UseIdentityServer();
 
-app.MapGet("/", () => "Hello World!");
+app.UseAuthorization();
+
+// Map endpoints / UI / controllers
+app.MapRazorPages();
+app.MapDefaultControllerRoute();
 
 app.Run();
